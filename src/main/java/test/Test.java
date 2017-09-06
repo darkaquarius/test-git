43dd5bdd6ce9401c134bcf34696264ba296bd508
huishen <darkaquarius88@163.com>
2017-09-06 19:53:08 +0800

    first commit

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Created by huishen on 17/9/6.
+ *
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
