6be4f647266b7e3ae17dce2fd180bc5dc0a2afe6
huishen <darkaquarius88@163.com>
2017-09-20 17:05:15 +0800

    hello world

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -9,15 +9,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("this is master");
-    }
-
-    public static void test() {
-        System.out.println("this is dev");
-    }
-
-    public static void test2() {
-        System.out.println("this is dev2");
     }
 
 }
