4f640158ef1c9dc1be6e08f3065bc44d9fc224d1
huishen <darkaquarius88@163.com>
2017-10-19 14:17:33 +0800

    master2

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -10,6 +10,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("master");
+        System.out.println("master2");
     }
 
 }
