459c84b0bedd16d4af8f207d9a4e406e1d680d94
huishen <darkaquarius88@163.com>
2017-10-19 14:24:27 +0800

    Revert "master2"
    This reverts commit 4f640158ef1c9dc1be6e08f3065bc44d9fc224d1.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -10,7 +10,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("master");
-        System.out.println("master2");
     }
 
 }
