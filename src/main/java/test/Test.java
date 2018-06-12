4e8f9c5afb850cc03cf24fc89d703ca1715691fb
huishen <darkaquarius88@163.com>
2018-06-12 11:33:20 +0800

    reset

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -9,31 +9,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("master");
-    }
-
-    public static void master2() {
-        System.out.println("master2");
-    }
-
-    public static void master4() {
-        System.out.println("master4");
-    }
-
-    public static void master5() {
-        System.out.println("master5");
-    }
-
-    public static void master6() {
-        System.out.println("master6");
-    }
-
-    public static void master7() {
-        System.out.println("master7");
-    }
-
-    public static void master8() {
-        System.out.println("master8");
     }
 
 }
